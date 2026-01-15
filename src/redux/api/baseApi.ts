064e027666b75df9f29e69112c4716bbc97f5601
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-01-15 09:54:25 +0600

    fix: update baseUrl in baseApi to use NEXT_PUBLIC_BACKEND_URL

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
   }),
   tagTypes: ["User"],
   endpoints: () => ({}),
